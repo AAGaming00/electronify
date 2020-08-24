42db36712e2fed3b39c05631b9569ef55a8bc8bf
AAGithub <aa@bigdumb.gq>
2020-08-24 10:18:56 -0400

    jank begone

--- tweaks/mini.js
+++ tweaks/mini.js
@@ -21,22 +21,24 @@ button.parentNode.replaceChild(new_element, button);
 button = document.querySelector(".now-playing__pip-toggle");
 // 3. Add event handler
 let olddim = new Object();
-let resetinterval = new Object();
 let win;
 let browser;
-function unmaximize(e) {
-    e.preventDefault()
-    browser.unmaximize()
-}
 const width = 1280;
 const minWidth = 800;
 const height = 120;
+function easeInOutExpo(x) {
+    return x === 0
+        ? 0
+        : x === 1
+            ? 1
+            : x < 0.5 ? Math.pow(2, 20 * x - 10) / 2
+                : (2 - Math.pow(2, -20 * x + 10)) / 2;
+}
 button.addEventListener ("click", async function() {
     win = remote.getCurrentWindow()
     browser = win.webContents.getOwnerBrowserWindow()
     if (document.querySelector('.miniplayer')) {
         console.log(olddim)
-        win.removeListener('maximize', unmaximize, false);
         setTimeout(() => browser.setMaximumSize(100000000, 100000000), 200); // why cant i just set it false?
         browser.setMinimumSize(olddim.minWidth, olddim.minHeight)
         setTimeout(() => browser.setSize(olddim.width, olddim.height), 300); // electron bug
@@ -54,32 +56,30 @@ button.addEventListener ("click", async function() {
         document.body.classList.add('miniplayer')
         document.body.classList.add('miniplayerStarting')
         document.body.classList.add('miniplayerStart')
-        await sleep(700)
-        console.log(win)
         olddim.width = browser.getSize()[0]
         olddim.height = browser.getSize()[1]
         olddim.minWidth = browser.getMinimumSize()[0]
         olddim.minHeight = browser.getMinimumSize()[1]
-        browser.setMinimumSize(width, height)
-        setTimeout(() => browser.setMaximumSize(100000000, height), 200); // :angery:
-        await sleep(100)
+        await sleep(600)
+        console.log(win)
         document.body.classList.remove('miniplayerStarting')
         document.body.classList.add('miniplayerStarted')
-        await sleep(100)
-        browser.setSize(width, height)
+        browser.setMinimumSize(olddim.minWidth, height)
+        browser.setSize(olddim.width, height)
+        browser.setMaximumSize(100000000, height)
         await sleep(200)
         document.body.classList.remove('miniplayerStarted')  
-        resetinterval.maximize = win.on('maximize',unmaximize, false);
     }
 });
 }
 function start() {
     const win = remote.getCurrentWindow()
     const browser = win.webContents.getOwnerBrowserWindow()
+    if (browser.getSize()[1] < 600) {
     setTimeout(() => browser.setMaximumSize(100000000, 100000000), 200); // why cant i just set it false?
     browser.setMinimumSize(800, 600)
     setTimeout(() => browser.setSize(1280, 720) , 300); // why
-
+    }
 
 }
 start()
